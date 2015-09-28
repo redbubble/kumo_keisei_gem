7f744ed0fdfde0d96cf5acabd3a718c0151d9182
Craig Read <craigabread@gmail.com>
2015-09-28 15:54:34 +1000

    added exit_status_for

--- lib/kumo_keisei/bash.rb
+++ lib/kumo_keisei/bash.rb
@@ -5,5 +5,10 @@ module KumoKeisei
       raise "unexpected exit code: #{$?.exitstatus} when running #{command}: #{output}" unless $?.exitstatus == 0
       output
     end
+
+    def exit_status_for(command)
+      `#{command}`
+      $?.exitstatus
+    end
   end
 end
--- spec/lib/kumo_keisei/bash_spec.rb
+++ spec/lib/kumo_keisei/bash_spec.rb
@@ -3,12 +3,25 @@ require 'kumo_keisei/bash'
 
 describe KumoKeisei::Bash do
 
-  it "runs bash and returns output" do
-    expect(KumoKeisei::Bash.new.execute("echo 1")).to eq("1")
+  describe "#execute" do
+
+    it "runs bash and returns output" do
+      expect(KumoKeisei::Bash.new.execute("echo 1")).to eq("1")
+    end
+
+    it 'raises an exception if returned a non-zero exit code' do
+      expect { KumoKeisei::Bash.new.execute('false') }.to raise_error(RuntimeError)
+    end
   end
 
-  it 'raises an exception if returned a non-zero exit code' do
-    expect { KumoKeisei::Bash.new.execute('false') }.to raise_error(RuntimeError)
-  end
+  describe "#exit_status_for" do
 
+    it 'returns error codes' do
+      expect(KumoKeisei::Bash.new.exit_status_for('false')).to eq(1)
+    end
+
+    it 'returns zero for sucessful commands' do
+      expect(KumoKeisei::Bash.new.exit_status_for('true')).to eq(0)
+    end
+  end
 end
