625efd9e5f8146f708873524269f0da9eed8c96e
Ricky Cook <ricky.cook@redbubble.com>
2015-12-31 13:00:47 +1100

    Fix gem versioning

--- script/build.sh
+++ script/build.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+if [[ -z "$KUMO_KEISEI_VERSION" && -n "$BUILDKITE_BUILD_NUMBER" ]]; then
+  export KUMO_KEISEI_VERSION="$BUILDKITE_BUILD_NUMBER"
+fi
+
 echo "--- :wind_chime: Building gem :wind_chime:"
 
 gem build kumo_keisei.gemspec
